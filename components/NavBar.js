cef51c8fd3a397a6b98e91afc0cf7566a9c0aafa
Oswaldo Rangel Suarez <orangel@tiempodevelopment.com>
2021-01-19 17:56:09 -0600

    first commit

---
+++ components/NavBar.js
@@ -0,0 +1,11 @@
+function Navigation() {
+  return (
+    <nav className="w-screen height-9 bg-indigo-400 flex justify-center space-x-4">
+      <li>test1</li>
+      <li>test2</li>
+      <li>test3</li>
+    </nav>
+  )
+}
+
+export default Navigation;
